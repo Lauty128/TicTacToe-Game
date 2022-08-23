b2f8f56c7feb7d4ccd69abd44c5fd9ef5b13644e
Lauty128 <lautarosilverii@gmail.com>
2022-08-23 07:47:30 -0300

    v2

--- script.js
+++ script.js
@@ -6,12 +6,25 @@ let icons = {
     </div>`,
     "circleIcon": `<div class="circleIcon iconOpacity"></div>`
 }
+let moves = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+]
+
 
 class game {
     constructor(){
         this.xUser=  "xIcon";
         this.circleUser= "circleIcon";
         this.currentUser = this.xUser;
+        this.movesAmount=0
+        this.winningUser="empate"
     }
 
     printSelection(grid){
@@ -25,6 +38,33 @@ class game {
         else this.currentUser = this.xUser
     }
 
+
+    play(){
+        let result = false
+        let amount;
+        moves.forEach(move=>{
+            amount = 0;
+            move.forEach(index=>{
+                if(document.getElementsByClassName(`square${index}`)[0].firstElementChild && document.getElementsByClassName(`square${index}`)[0].firstElementChild.classList.contains(this.currentUser)){
+                    amount++
+                }
+            })
+            if(amount == 3){
+                result = true
+                return
+            }
+        })
+        return result
+    }
+
+    restart(){
+        for (let index = 0; index < document.getElementsByClassName("square").length -1; index++) {
+            document.getElementsByClassName("square")[index].innerHTML = ""
+        }
+        this.currentUser = this.xUser;
+        this.movesAmount = 0
+        this.winningUser = "empate"
+    }
     
 
 }
@@ -42,13 +82,22 @@ gameGrid.addEventListener("mouseover",e=>{
         }
 
         e.target.addEventListener("click",()=>{
+            let result;
             if(!e.target.classList.contains("select")){
                 actionsGame.printSelection(e.target)
+                actionsGame.movesAmount += 1
+                if(actionsGame.movesAmount >= 5 && actionsGame.play()){
+                    console.log("GANO: ", actionsGame.currentUser)
+                }
+                //FUNCION QUE CONTROLE SI HAY UNA LINEA DE TRES IGUALES
                 actionsGame.toggleUser()
             }
-            //FUNCION QUE CONTROLE SI HAY UNA LINEA DE TRES IGUALES
         })
     }
 
 
+})
+
+document.getElementById("restartButton").addEventListener("click",()=>{
+    actionsGame.restart()
 })
